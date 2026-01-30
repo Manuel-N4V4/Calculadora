c657a5f3844d22c4aa2532197b043f9d0e6c5afa
Nava <210636510+Manuel-N4VA@users.noreply.github.com>
2026-01-29 19:00:59 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora/Form1.cs
@@ -0,0 +1,138 @@
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        double primero;
+        double segundo;
+        double resultado;
+        string operacion;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "7";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "4";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "1";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "0";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "8";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "5";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "9";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "6";
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Pantalla.Clear();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "3";
+        }
+
+        private void btnPunto_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += ".";
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            operacion = "/";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            operacion = "*";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+            operacion = "-";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void btnMas_Click(object sender, EventArgs e)
+        {
+            operacion = "+";
+            primero = double.Parse(Pantalla.Text);
+            Pantalla.Clear();
+        }
+
+        private void btnRaiz_Click(object sender, EventArgs e)
+        {
+            operacion = "Raiz";
+            primero = double.Parse(Pantalla.Text);
+            resultado = primero;
+            Pantalla.Text = Math.Sqrt(resultado).ToString();
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            segundo = double.Parse(Pantalla.Text);
+
+            switch(operacion)
+            {
+                case "+":
+                    resultado = primero + segundo;
+                    Pantalla.Text = resultado.ToString();
+                    break;
+                case "-":
+                    resultado = primero - segundo;
+                    Pantalla.Text = resultado.ToString();
+                    break;
+                case "*":
+                    resultado = primero * segundo;
+                    Pantalla.Text = resultado.ToString();
+                    break;
+                case "/":
+                    resultado = primero / segundo;
+                    Pantalla.Text = resultado.ToString();
+                    break;
+                case "Raiz":
+
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Pantalla.Text += "2";
+        }
+    }
+}
